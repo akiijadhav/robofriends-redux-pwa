cda4503c0023e531acad006d018ce7275f9d417d
akiijadhav <akiijadhav22@gmail.com>
2021-02-19 03:19:09 +0530

    reverting

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css'; 
@@ -25,4 +25,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+registerServiceWorker();
